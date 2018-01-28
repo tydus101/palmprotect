325615bc63ecaf5ed06bf0233938ab115284942c
Tyler Reimold <trr4652@rit.edu>
2018-01-27 23:46:13 -0500

    Playing with animations

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,20 +2,23 @@ var palm_app = angular.module('palmApp', ['ngMaterial', 'ngAnimate']);
 
 palm_app.controller('palmAppController', function ($scope, $http) {
 
-var parseNdbId = function (id) {
+    // Is the first ground in the page hidden?
+    $scope.groundOneHidden = false;
+    var parseNdbId = function (id) {
     var result = $http.get('https://api.nal.usda.gov/ndb/reports/?' +
         'ndbno=' + id + '&' +
         'type=b&' +
         'format=json&' +
         'api_key=YyAhPMz3MUxL5X1r2jhdo5rFtavN95Iqe7wNVCHL').then(function (value) {
+            
             console.log(value)
     })
-};
+    };
 
 
 
-$scope.searchFood = function (term) {
-    var search = $http.get('https://api.nal.usda.gov/ndb/search/?' +
+    $scope.searchFood = function (term) {
+        var search = $http.get('https://api.nal.usda.gov/ndb/search/?' +
         'format=json&' +
         'q=' + term + '&' +
         'max=25&' +
@@ -24,7 +27,7 @@ $scope.searchFood = function (term) {
         .then(function (result) {
             console.log(result);
             parseNdbId(result.data.list.item[0].ndbno);
-    })
+        })
 
 }
 

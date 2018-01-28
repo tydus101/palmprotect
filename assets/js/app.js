d4228452b4297648dc966066a74cec93ec25e5e9
Tyler Reimold <trr4652@rit.edu>
2018-01-28 04:53:15 -0500

    Added for finding palm content

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,38 @@
 var palm_app = angular.module('palmApp', ['ngMaterial', 'ngAnimate']);
 
 palm_app.controller('palmAppController', function ($scope, $http, $location) {
-    var isHappyOrSad = function () {
-        $scope.isHappyOrSad = function () {
-            if(hasPalmOil(value)){
-                return "true";
-            }
-            else{
-                return "false";
-            }
-        }
-    };
     // Is the first ground in the page hidden?
     params = $location.search();
+    // Does the product have palm oil?
+    $scope.hasPalm = false;
+    console.log(params);
+    // If there
+    var parseNdbId = function (id) {
+        var result = $http.get('https://api.nal.usda.gov/ndb/reports/?' +
+            'ndbno=' + id + '&' +
+            'type=b&' +
+            'format=json&' +
+            'api_key=YyAhPMz3MUxL5X1r2jhdo5rFtavN95Iqe7wNVCHL').then(function (value) {
+            $scope.currentPage = 'selected';
+            $location.search('dbid', id);
+            $scope.hasPalm = hasPalmOil(value);
+            console.log($scope.hasPalm);
+            console.log($scope.currentPage);
+        })
+    };
+
+
+
+
     if (params.dbid) {
+        parseNdbId(params.dbid);
         $scope.currentPage = 'selected';
-        isHappyOrSad();
     }
     else{
         $scope.currentPage = 'start';
     }
 
 
-    // If there
-    var parseNdbId = function (id) {
-    var result = $http.get('https://api.nal.usda.gov/ndb/reports/?' +
-        'ndbno=' + id + '&' +
-        'type=b&' +
-        'format=json&' +
-        'api_key=YyAhPMz3MUxL5X1r2jhdo5rFtavN95Iqe7wNVCHL').then(function (value) {
-            $scope.currentPage = 'selected';
-            $location.search('dbid', id);
-            console.log(value);
-            //console.log(value)
-        console.log(productName(value));
-        console.log(hasPalmOil(value));
-    })
-    };
 
 
 var productName = function (json){

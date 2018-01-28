f531c746bb2b9f2a4b485bfaf7c640181fe94c46
Tyler Reimold <trr4652@rit.edu>
2018-01-27 21:33:21 -0500

    Add beginning of search function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,21 @@
 var palm_app = angular.module('palmApp', ['ngMaterial']);
 
-palm_app.controller('palmAppController', function ($scope) {
+palm_app.controller('palmAppController', function ($scope, $http) {
 
 
 $scope.test = "Hi";
+
+$scope.searchFood = function (term) {
+    var search = $http.get('https://api.nal.usda.gov/ndb/search/?' +
+        'format=json&' +
+        'q=' + term + '&' +
+        'max=25&' +
+        'api_key=YyAhPMz3MUxL5X1r2jhdo5rFtavN95Iqe7wNVCHL&' +
+        'ds=Branded Food Products')
+        .then(function (result) {
+            console.log(result)
+    })
+}
 
 
 
